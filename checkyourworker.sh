7d08a1f6b1fb8eed23d31da751d0342ca218733e
Ramazan Ozbilen <62715649+rozbilen@users.noreply.github.com>
2024-07-15 19:22:50 +0300

    Update checkyourworker.sh

--- checkyourworker.sh
+++ checkyourworker.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+# Prompt for topic input
+read -p "Enter topic (default: allora-topic-1-worker): " TOPIC
+TOPIC=${TOPIC:-allora-topic-1-worker}
+
+# Parse TOPIC_ID
+TOPIC_ID=$(echo "$TOPIC" | awk -F'-' '{print $3}')
+
+# Determine the token based on TOPIC_ID
+case "$TOPIC_ID" in
+    1) TOKEN="eth" ;;
+    3) TOKEN="btc" ;;
+    5) TOKEN="sol" ;;
+    *) TOKEN="eth" ;; # Default action set to ETH for invalid TOPIC_ID
+esac
+
+# Get the current block height
 block_height=$(curl -s https://allora-rpc.edgenet.allora.network/block | jq -r .result.block.header.height)
+
+# Perform the curl request with the parsed topic and block height
 response=$(curl --silent --location 'http://localhost:6000/api/v1/functions/execute' \
 --header 'Content-Type: application/json' \
 --data '{
     "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",
     "method": "allora-inference-function.wasm",
     "parameters": null,
-    "topic": "1",
+    "topic": "'$TOPIC_ID'",
     "config": {
         "env_vars": [
             {
@@ -15,7 +33,7 @@ response=$(curl --silent --location 'http://localhost:6000/api/v1/functions/exec
             },
             {
                 "name": "ALLORA_ARG_PARAMS",
-                "value": "ETH"
+                "value": "'$TOKEN'"
             },
             {
                 "name": "ALLORA_BLOCK_HEIGHT_CURRENT",
@@ -27,5 +45,7 @@ response=$(curl --silent --location 'http://localhost:6000/api/v1/functions/exec
     }
 }')
 
+# Print the response
 echo "Response:"
 echo "$response" | jq .
+
